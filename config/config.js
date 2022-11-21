7f6939d20cf61708ac3077954d4d97f919d68f62
Zaid Abdillah <zaidabdillah18@gmail.com>
2022-11-21 23:25:47 +0700

    upload env

--- config/config.js
+++ config/config.js
@@ -17,11 +17,12 @@ const {DB_PORT,DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME,DB_DILALECT } = process.e
     dialect: "mysql"
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    port : DB_PORT,
+    dialect: DB_DILALECT
   }
 }
   
