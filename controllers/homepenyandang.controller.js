f877c71ed1c06fc7519757ca70a4c78ea8987680
Zaid Abdillah <zaidabdillah18@gmail.com>
2022-11-22 14:59:52 +0700

    home penyandang

--- controllers/homepenyandang.controller.js
+++ controllers/homepenyandang.controller.js
@@ -11,8 +11,8 @@ async function homepenyandang(req,res){
         const homekategoripenyandang = await models.KategoriProgram.findAll({
             attributes: ['id','nama','deskripsi','gambar'],
             include:[{
-                models:Program,
-                include: [DataMitra]
+                models: models.Program,
+                include: [models.DataMitra]
                 }
             ]
           })
