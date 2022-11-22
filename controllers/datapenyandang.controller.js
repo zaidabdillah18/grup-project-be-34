823bedb888cf0ab3cd9f3a63126302b2e76ed0f1
Zaid Abdillah <zaidabdillah18@gmail.com>
2022-11-23 03:47:24 +0700

    program penyandang

--- controllers/datapenyandang.controller.js
+++ controllers/datapenyandang.controller.js
@@ -26,14 +26,62 @@ async function createDatapribadi (req, res) {
     })
 }
 }
+async function getDatapribadi(req,res){
+  const auth = await req.headers.authorization
+  const token = await auth.split(' ')[1]
+  const verified = jwt.verify(token, 'secret')
+  if (verified.posisi === "penyandang disabilitas no-lsm" || verified.posisi === "penyandang disabilitas lsm") {
+    const ambil = await models.DataPenyandang.findAll({
+      where: {
+        id: verified.id_user
+      }
+    })
+    res.status(200).json({
+      message: 'Success show data',
+      data: ambil
+    })
+  } else {
+    res.status(500).json({
+      message: "Invalid credentials!",
+    });
+  }
+}
+async function editDatapribadi(req,res){
+  const auth = await req.headers.authorization
+  const token = await auth.split(' ')[1]
+  const verified = jwt.verify(token, 'secret')
+  if (verified.posisi === "penyandang disabilitas no-lsm" || verified.posisi === "penyandang disabilitas lsm") {
+    
+    const ambil = await models.DataPenyandang.update({
+      nama: req.body.nama,
+      // nama: req.body.jenis_kelamin,
+      // nama: req.body.tempat_lahir,
+      // nama: req.body.tanggal_lahir,
+      // nama: req.body.agama,
+      // nama: req.body.nik
+    },
+    {
+      where: {
+        id: verified.id_user
+      }
+    })
+    res.status(200).json({
+      message: 'Success show data',
+      data: ambil
+    })
+  } else {
+    res.status(500).json({
+      message: "Invalid credentials!",
+    });
+  }
+}
 async function createkontakpribadi (req,res){
   const auth = await req.headers.authorization
   const token = await auth.split(' ')[1]
 
   const verified = jwt.verify(token, 'secret')
   if (verified.posisi === "penyandang disabilitas no-lsm" || verified.posisi === "penyandang disabilitas lsm") {
-    const id = req.params.id
-    const idpenyandang = await models.DataPenyandang.findByPk(id);
+
     const temp = await models.KontakPribadi.create({
       no_hp: req.body.no_hp,
       alamat: req.body.alamat,
@@ -42,7 +90,7 @@ async function createkontakpribadi (req,res){
       kecamatan: req.body.kecamatan,
       desa: req.body.desa,
       kode_pos: req.body.kode_pos,
-      id_datapenyandang: idpenyandang.id
+      id_datapenyandang: verified.id_user
     })
     res.json({
       status: 200,
@@ -58,13 +106,9 @@ async function creatuploadberkas(req,res){
 
   const verified = jwt.verify(token, 'secret')
   if (verified.posisi === "penyandang disabilitas no-lsm" || verified.posisi === "penyandang disabilitas lsm") {
-  
-  const id = req.params.id
-  const idpenyandang = await models.DataPenyandang.findByPk(id); 
-
   const temp = await models.UploadBerkas.create({
     file_ktp: req.file.filename,
-    id_datapenyandang: idpenyandang.id
+    id_datapenyandang: verified.id_user
   })
   res.json({
     status: 200,
@@ -73,148 +117,11 @@ async function creatuploadberkas(req,res){
   })
   }
 }
-// async function getDataPenyandang (req, res) {
-//   const auth = await req.headers.authorization
-//   const token = await auth.split(' ')[1]
 
-//   const verified = jwt.verify(token, 'secret')
-//   const ambil = await DataPenyandang.findAll({
-//     where: {
-//       user_id: verified.user_id
-//     }
-//   })
-//   res.status(200).json({
-//     message: 'Success get all data',
-//     data: ambil
-//   })
-// }
-// async function getDataPenyandangbyid (req, res) {
-//   const auth = await req.headers.authorization
-//   const token = await auth.split(' ')[1]
-
-//   const verified = jwt.verify(token, 'secret')
-
-//   const id = req.params.id
-//   // check id in table todolist
-//   let ambilid = await TODO.findByPk(id)
-//   if (!ambilid) {
-//     res.status(404).json({ status: 404, message: 'Data not found' })
-//   } else {
-//     if (ambilid.user_id == verified.user_id) {
-//       res.status(200).json({ message: 'Success get data', data: ambilid })
-//     } else {
-//       res.status(400).json({ message: 'failed get data' })
-//     }
-//   }
-// }
-// async function editTODO (req, res) {
-//   const auth = await req.headers.authorization
-//   const token = await auth.split(' ')[1]
-
-//   const verified = jwt.verify(token, 'secret')
-//   // validation
-//   const schema = {
-//     name: 'string|optional',
-//     description: 'string|optional'
-//   }
-//   const validate = v.validate(req.body, schema)
-//   if (validate.length) {
-//     return res.status(400).json(validate)
-//   }
-//   const id = req.params.id
-//   let note = await TODO.findByPk(id)
-//   if (!note) {
-//     res.status(404).json({ status: 404, message: 'Data not found' })
-//   } else {
-//     if (note.user_id == verified.user_id) {
-//       // proses update
-//       edit = await TODO.update(
-//         {
-//           name: req.body.name,
-//           description: req.body.description,
-//           isDone: req.body.isDone
-//         },
-//         {
-//           where: {
-//             id: id
-//           }
-//         }
-//       )
-//       res.json({
-//         status: 200,
-//         message: 'Success update data',
-//         data: edit
-//       })
-//     } else {
-//       res.json({
-//         status: 400,
-//         message: 'Failed update data'
-//       })
-//     }
-//   }
-// }
-// async function hapusTODObyid (req, res) {
-//   const auth = await req.headers.authorization
-//   const token = await auth.split(' ')[1]
-
-//   const verified = jwt.verify(token, 'secret')
-//   const id = req.params.id
-//   // check id in table todolist
-//   let hapus = await TODO.findByPk(id)
-//   if (!hapus) {
-//     return res.status(404).json({ status: 404, message: 'Data not found' })
-//   }
-//   if (hapus.user_id == verified.user_id) {
-//     // proses delete data
-//     const hapus1 = await TODO.destroy({
-//       where: {
-//         id: id
-//       }
-//     })
-//     res.json({
-//       status: 200,
-//       message: 'Success delete data',
-//       data: hapus1
-//     })
-//   } else {
-//     res.json({
-//       status: 400,
-//       message: 'failed delete data'
-//     })
-//   }
-// }
-// async function hapusTODO (req, res) {
-//   // proses delete data
-//   const auth = await req.headers.authorization
-//   const token = await auth.split(' ')[1]
-
-//   const verified = jwt.verify(token, 'secret')
-
-//   if (!verified) {
-//     res.json({
-//       status: 400,
-//       message: 'failed delete data'
-//     })
-//   } else {
-//     const hapusid = await TODO.destroy({
-//       where: {
-//         user_id: verified.user_id
-//       }
-//     })
-//     res.json({
-//       status: 200,
-//       message: 'Success delete data',
-//       data: hapusid
-//     })
-//   }
-// }
 module.exports = {
   createDatapribadi: createDatapribadi,
+  getDatapribadi:getDatapribadi,
+  editDatapribadi:editDatapribadi,
   createkontakpribadi:createkontakpribadi,
   creatuploadberkas:creatuploadberkas
-//   getDataPenyandang: getDataPenyandang,
-//   getTODObyid: getTODObyid,
-//   editTODO: editTODO,
-//   hapusTODObyid: hapusTODObyid,
-//   hapusTODO: hapusTODO
 }
--- routes/datapenyandang.js
+++ routes/datapenyandang.js
@@ -26,8 +26,11 @@ const fileStroge = multer.diskStorage({
     fileFilter: fileFilter
   })
 router.post("/datapribadi", datapenyandangController.createDatapribadi);
-router.post("/datapribadi/kontakpribadi/:id", datapenyandangController.createkontakpribadi);
-router.post("/datapribadi/kontakpribadi/upload_berkas/:id", upload.single('file_ktp') ,datapenyandangController.creatuploadberkas);
+router.get("/datapribadi", datapenyandangController.getDatapribadi);
+router.put("/datapribadi", datapenyandangController.editDatapribadi);
+
+router.post("/datapribadi/kontakpribadi", datapenyandangController.createkontakpribadi);
+router.post("/datapribadi/upload_berkas", upload.single('file_ktp') ,datapenyandangController.creatuploadberkas);
 
 //login
 // router.post("/lihat", datapenyandangController.lihat)
