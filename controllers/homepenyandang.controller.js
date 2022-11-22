212374052dae26e44810f5d24f6ba267982c7f71
Zaid Abdillah <zaidabdillah18@gmail.com>
2022-11-22 11:55:06 +0700

    home penyandang

--- controllers/homepenyandang.controller.js
+++ controllers/homepenyandang.controller.js
@@ -8,13 +8,20 @@ async function homepenyandang(req,res){
     const verified = jwt.verify(token, 'secret') 
    
     if (verified.posisi === "penyandang disabilitas no-lsm" || verified.posisi === "penyandang disabilitas lsm") {
-        const detailprogram = await models.Program.findAll({
+        const id = req.params.id
+        const detailprogram = await models.KategoriProgram.findAll({
+            include: [
+              { 
+                model: models.Program, as: 'Programs',
+                attributes: ['id','nama','deskripsi']
+            }
+            ],
             include:[{
                 model: models.DataMitra, as: 'DataMitras',
-                attributes: ['id','nama']
+                attributes: ['id','nama_mitra']
             }],
-            attributes: ['id','nama','deskripsi','gambar','status_program']
-         })
+            attributes: ['id','nama','deskripsi','gambar','status_program'],
+          })
           res.status(200).json({
             message: 'Success show data',
             data: detailprogram
